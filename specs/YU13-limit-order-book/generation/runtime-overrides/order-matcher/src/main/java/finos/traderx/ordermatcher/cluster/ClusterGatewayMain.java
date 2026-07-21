2ca90189c5b673b94373af2e7053025abc850b4f
yseif02 <aseif@mail.yu.edu>
2026-07-21 19:29:42 -0400

    YU13: raise gateway Aeron term buffers to 4M

--- specs/YU13-limit-order-book/generation/runtime-overrides/order-matcher/src/main/java/finos/traderx/ordermatcher/cluster/ClusterGatewayMain.java
+++ specs/YU13-limit-order-book/generation/runtime-overrides/order-matcher/src/main/java/finos/traderx/ordermatcher/cluster/ClusterGatewayMain.java
@@ -212,9 +212,9 @@ public final class ClusterGatewayMain implements OrderSubmitter {
                 CloseHelper.quietClose(client);
                 client = AeronCluster.connect(new AeronCluster.Context()
                     .aeronDirectoryName(aeronDir)
-                    .ingressChannel("aeron:udp?term-length=1m")
+                    .ingressChannel("aeron:udp?term-length=4m")
                     .ingressEndpoints(entry)
-                    .egressChannel("aeron:udp?term-length=1m|endpoint="
+                    .egressChannel("aeron:udp?term-length=4m|endpoint="
                         + env("GATEWAY_EGRESS_HOST", env("POD_IP", "localhost")) + ":"
                         + env("GATEWAY_EGRESS_PORT", "0"))
                     .egressListener(this::onEgress));
